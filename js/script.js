08569115d2dd4b278c0d5a35dc9fecd0169ff538
DeversGreenGV <86885053+DeversGreenGV@users.noreply.github.com>
2022-07-27 18:15:09 +0300

    Sprit12 (Slider and decor)
    Добавлены декоративные элементы (hr`ы). Исправлены недочеты в стилях (в основном был исправлен выход контента секции за ее пределы ). В секции Our_team реализован слайдер

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function name(params) {
+    $('.Our_team .list').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false,
+        dots: true
+
+    });
+})
