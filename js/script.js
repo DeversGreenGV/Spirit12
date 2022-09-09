9dc556a2a9efd3994404e143190176c8716238d8
DeversGreenGV <86885053+DeversGreenGV@users.noreply.github.com>
2022-09-09 10:42:26 +0300

    Sprit12 ( adaptive - mobile ) 0.4.1.4

--- js/script.js
+++ js/script.js
@@ -64,8 +64,8 @@
             {
                 breakpoint: 700,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
                 }
             },
 
